ae49c0c9c6038a5de346e8163738314d35b8f6c9
chenxiang_lendingcloud <chenxiang@lending-cloud.com>
2021-02-22 15:04:08 +0800

    bind

---
+++ bind.js
@@ -0,0 +1,48 @@
+/**
+ * 参考链接 https://www.cnblogs.com/echolun/p/12178655.html
+ */
+
+Function.prototype.cxBind = function (obj) {
+  if (typeof this !== 'function') {
+    throw new Error(
+      'Function.prototype.bind - what is trying to be bound is not callable'
+    );
+  }
+
+  const fn = this;
+  // 获取到函数的参数
+  const args1 = [].slice.call(arguments, 1);
+  //原型链继承 这里不能直接继承 需要用一个新函数
+  const _fn = function () {};
+  const newFn = function () {
+    // 这里获取到bind返回的函数的参数
+    const arg2 = [].slice.call(arguments);
+    //通过constructor判断调用方式，为true this指向实例，否则为obj
+    fn.apply(_fn.prototype.isPrototypeOf(this) ? this : obj, [
+      ...args1,
+      ...arg2,
+    ]);
+  };
+
+  _fn.prototype = fn.prototype;
+  newFn.prototype = new _fn();
+  return newFn;
+};
+
+const obj = {
+  n: 1,
+};
+
+function fn(x, y, z) {
+  this.name = 'name1';
+  console.log(this.n);
+  console.log(x);
+  console.log(y);
+  console.log(z);
+}
+
+const fn2 = fn.cxBind(obj, 2).cxBind(obj, 3);
+
+fn2(4); // 1 2 3 4
+
+new fn2(4); // undefined 2 3 4
